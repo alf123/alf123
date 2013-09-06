e78aab27d8975c52d4a4aa14bf977692ef9bad15
alf123 <fpytainan@gmail.com>
2013-09-06 11:59:51 +0800

    part of persistence task
    part 1, prototyping stock persistence

--- src/smallfu.py
+++ src/smallfu.py
@@ -1,9 +1,41 @@
 from PyDbLite import Base
 import datetime
 today = datetime.date.today()
-impactDB = Base("F://alfStock//"+str(today)+'.yv')
-impactDB.open()
-sammi =([r for r in impactDB if r['UpOrDown']=="U"])
+#impactDB = Base("F://alfStock//"+str(today)+'.yv')
+#impactDB.open()
+#sammi =([r for r in impactDB if r['UpOrDown']=="U"])
+#print len(sammi)
+#for d in sammi:
+#    print d['sid']
 
-for d in sammi:
-    print d['sid']
+
+#AintB update Current length + 1
+#historyDB = Base("F://alfStock//"+"alf123"+'.history')
+
+#historyDB.create('sid','Edate', 'lenth')
+#historyDB.open()
+
+#A-B History: sid date length
+#currentDB = Base("F://alfStock//"+"alf123"+'.current')
+
+#currentDB.create('sid','Edate', 'lenth')
+#currentDB.open()
+
+#B-A insert Current: sid date length
+
+def resetHisDB():
+    historyDB = Base("F://alfStock//"+"alf123"+'.history')
+    historyDB.create('sid','Edate', 'length')
+    historyDB.open()
+    historyDB.commit()
+    currentDB = Base("F://alfStock//"+"alf123"+'.current')
+    currentDB.create('sid','Edate', 'length')
+    currentDB.open()
+    currentDB.commit()
+
+def UpStill(entry):
+    record = [r for r in currentDB if r['sid'] == entry['sid']]
+    if len(r)>0:
+        k = record['length']
+        currentDB.update(record,length=k+1)
+resetHisDB()
